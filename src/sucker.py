935705ad456642b12468c26f78f985b23b03b3d6
Konstantin Vanyushov <KVanyushov@gmail.com>
2019-02-01 10:20:56 +1100

    CORS request handling

--- src/sucker.py
+++ src/sucker.py
@@ -8,7 +8,7 @@ app = Bottle()
 
 @app.hook('after_request')
 def enable_cors():
-    response.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
     response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
 
